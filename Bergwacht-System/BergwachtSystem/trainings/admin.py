06ce886f9e40aebab89384be98d105248165d6d7
Peter Zarnitz <peter.zarnitz@me.com>
2018-03-19 14:09:29 +0100

    TimsStampAdmin

--- Bergwacht-System/BergwachtSystem/trainings/admin.py
+++ Bergwacht-System/BergwachtSystem/trainings/admin.py
@@ -4,9 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from models import Training, Prerequisites_for_training, TrainingCategory, TrainingEvent, Participates_in_trainingevent, Possible_participates_in_trainingevent
 
+
+class TimeStampAdmin(admin.ModelAdmin):
+    readonly_fields = ('first_register_timestamp',)
+
+
 admin.site.register(Training)
 admin.site.register(Prerequisites_for_training)
 admin.site.register(TrainingCategory)
 admin.site.register(TrainingEvent)
-admin.site.register(Participates_in_trainingevent)
-admin.site.register(Possible_participates_in_trainingevent)
+admin.site.register(Participates_in_trainingevent, TimeStampAdmin)
+admin.site.register(Possible_participates_in_trainingevent, TimeStampAdmin)
