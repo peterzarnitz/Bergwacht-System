c1da42644468cd0f9671e7ca0c57a8dddfdc7acc
Peter Zarnitz <peter.zarnitz@me.com>
2018-03-19 15:36:59 +0100

    trainings timestamp can be null

--- Bergwacht-System/BergwachtSystem/trainings/models.py
+++ Bergwacht-System/BergwachtSystem/trainings/models.py
@@ -79,7 +79,7 @@ class Participates_in_trainingevent(models.Model):
     trainingevent = models.ForeignKey(TrainingEvent, on_delete=models.CASCADE)
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     comment = models.TextField(verbose_name='Kommentar', max_length=200, blank=True)
-    first_register_timestamp = models.DateTimeField(verbose_name='Erste Anmeldung', editable=True)
+    first_register_timestamp = models.DateTimeField(verbose_name='Erste Anmeldung', editable=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Teilnahmen an Ausbildungstermin'
@@ -93,7 +93,7 @@ class Possible_participates_in_trainingevent(models.Model):
     trainingevent = models.ForeignKey(TrainingEvent, on_delete=models.CASCADE)
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     comment = models.TextField(verbose_name='Kommentar', max_length=200, blank=True)
-    first_register_timestamp = models.DateTimeField(default=datetime.now, verbose_name='Erste Anmeldung', editable=True)
+    first_register_timestamp = models.DateTimeField(default=datetime.now, verbose_name='Erste Anmeldung', editable=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Mögliche Teilnahmen an Ausbildungstermin'
